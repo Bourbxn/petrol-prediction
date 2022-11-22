11650b18ce959309e422e216363db7600573707c
Arn <arn.arn3328@gmail.com>
2022-11-22 16:28:24 +0700

    updated + fixed linear regression predict

--- app.py
+++ app.py
@@ -10,7 +10,14 @@ user_input = st.text_input('Enter Date (format: Y-M-d)', '2011-02-01')
 df = pd.read_csv("crude-oil-price.csv")
 df_origin = df
 
-# Prediction
+# Prediction with Linear Equation
+df_origin['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
+X_origin = df['date']
+y_origin = df['price'].values.reshape(-1, 1)
+model2 = LinearRegression().fit(X_origin.values.reshape(-1,1),y_origin)
+predictions = model2.predict(X_origin.values.astype(float).reshape(-1, 1))
+
+# Prediction with Past Price
 df['date'] = pd.to_datetime(df['date'])
 df['MA3'] = df['price'].shift(1).rolling(window=3).mean()
 df['MA9']= df['price'].shift(1).rolling(window=9).mean()
@@ -26,8 +33,8 @@ y_train = y[:t]
 X_test = X[t:]
 y_test = y[t:]
 
-model = LinearRegression().fit(X_train,y_train)
-predicted_price = model.predict(X_test)
+model1 = LinearRegression().fit(X_train,y_train)
+predicted_price = model1.predict(X_test)
 predicted_price = pd.DataFrame(predicted_price,index=y_test.index,columns = ['price'])
 df['predictedPrice'] = predicted_price
 
@@ -36,19 +43,25 @@ df = df.drop(columns='MA9', axis=6).copy()
 df = df.dropna()
 
 # User Input
-df_index = df.index
-index = df['date'] == user_input
-result = df_index[index]
+input_format = pd.to_datetime(user_input, format='%Y-%m-%d')
+user_pred = model2.predict(pd.DataFrame([input_format]).values.astype(float).reshape(-1, 1))
 
 # Display value
-st.subheader(f'Value of {user_input}')
-try:
-    st.write(df.loc[result.tolist()[0], ['date', 'price', 'predictedPrice']])
-except:
-    st.write("No data")
+st.subheader(f'Price of {user_input}')
+st.write('Predict Price :', user_pred[0,0])
 
-# Plotting
-st.subheader('Prediction vs Actual Price')
+# Linear Regression
+st.subheader('Prediction with Linear Equation')
+fig3 = plt.figure(figsize=(12,6))    
+plt.scatter(X_origin, y_origin, label='actual price', color='green')
+plt.plot(X_origin, predictions, label='prediction', linewidth=3)
+plt.xlabel('date')
+plt.ylabel('price')
+plt.legend()
+st.pyplot(fig3)
+
+# Plotting 
+st.subheader('Prediction Present Price with Past Price')
 fig2 = plt.figure(figsize=(12,6))
 plt.plot(predicted_price)
 plt.plot(y_test)
@@ -64,4 +77,4 @@ st.pyplot(fig)
 
 # Describing Data
 st.subheader('Data table')
-st.write(df)
+st.write(df_origin)
